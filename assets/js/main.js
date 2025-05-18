943479a74e92fd9e05e0d0938dc81aaf67dfce1c
aayushmdr <44645416+aayushmdr@users.noreply.github.com>
2025-05-18 15:51:08 +0545

    Added Swyper.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
-AOS.init();
-
 
 //For loader
-$(window).on("load", function(){
-    $(".loader-wrapper").fadeOut("slow");
-    $("body").removeClass("preload"); 
+// $(window).on("load", function(){
+//     $(".loader-wrapper").fadeOut("slow");
+//     $("body").removeClass("preload"); 
+// });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+   delay: 4000,
+ },
+ slidesPerView: 1,
+ spaceBetween: 0,
+//  effect: 'fade',
+//   fadeEffect: {
+//     crossFade: true
+//   },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
