841c37df679cf7d1ae83b037415673113119ca34
aayushmdr <44645416+aayushmdr@users.noreply.github.com>
2025-06-08 15:00:53 +0545

    Added new gallery page

---
+++ assets/js/gallery.js
@@ -0,0 +1,8 @@
+// Initialize MixItUp
+    var mixer = mixitup('#gallery');
+
+    // Optional: Fancybox config
+    Fancybox.bind('[data-fancybox="gallery"]', {
+      Thumbs: true,
+      Toolbar: true,
+    });
